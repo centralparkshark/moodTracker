8608e5734e7520869f02fafc4f17ef49dd6d9d17
Vinncent Miller <vinncentmiller@gmail.com>
2024-09-05 12:19:53 -0400

    Input test data

---
+++ data/journal.js
@@ -0,0 +1,32 @@
+const journal = [
+    {
+      id: 1,
+      userId: 1, 
+      mood: [
+        {
+            moodId: 1,
+            rating: 2,
+        },
+        {
+            moodId: 3,
+            rating: 4,
+        },
+    ]
+    },
+    {
+        id: 2,
+        userId: 1, 
+        mood: [
+          {
+              moodId: 1,
+              rating: 2,
+          },
+          {
+              moodId: 3,
+              rating: 4,
+          },
+      ]
+      },
+  ];
+  
+  module.exports = journal;
---
+++ data/moods.js
@@ -0,0 +1,20 @@
+const moods = [
+    {
+      id: 1,
+      name: "Happy",
+      details: "",
+    },
+    {
+      id: 2,
+      name: "Sad",
+      details: "",
+    },
+    {
+      id: 3,
+      name: "Angry",
+      details: "",
+    },
+  ];
+  
+  module.exports = moods;
+  
